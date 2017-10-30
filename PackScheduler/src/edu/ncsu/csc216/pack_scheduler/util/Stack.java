e6588149ce0c71023e6cf87a4496cd28b567aa5a
Brian Wu <bwu9@ncsu.edu>
2017-10-30 17:41:14 -0400

    added javadoc for stack interface

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/Stack.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/Stack.java
@@ -8,11 +8,42 @@ package edu.ncsu.csc216.pack_scheduler.util;
  *
  */
 public interface Stack<E> {
-    
+
+    /**
+     * Pushes an element to the top of the stack
+     * 
+     * @param element
+     *            the element we want to add to the stack
+     */
     void push(E element);
+
+    /**
+     * Pops off the element at the top of the stack
+     * 
+     * @return the element the user just popped off
+     */
     E pop();
+
+    /**
+     * Checks if the stack is empty
+     * 
+     * @return true if there is nothing in the stack, otherwise returns false
+     */
     boolean isEmpty();
+
+    /**
+     * Returns the number of elements in the stack
+     * 
+     * @return the number of elements in the stack
+     */
     int size();
+
+    /**
+     * Sets the capacity of the stack
+     * 
+     * @param capacity
+     *            the number you want to cap your stack at
+     */
     void setCapacity(int capacity);
-    
+
 }
