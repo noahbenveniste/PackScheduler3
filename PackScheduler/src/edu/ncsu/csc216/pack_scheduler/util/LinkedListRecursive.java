0a71c91f2a123832fe83d79f36a498ecc69e740a
noahbenveniste <benven.noa1995@gmail.com>
2017-11-08 10:52:43 -0500

    Created LinkedListRecursive skeleton

---
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedListRecursive.java
@@ -0,0 +1,29 @@
+package edu.ncsu.csc216.pack_scheduler.util;
+
+/**
+ * 
+ * @author Noah Benveniste
+ * @author Brian Wu
+ */
+public class LinkedListRecursive<E> {
+
+    /**
+     * 
+     */
+    public LinkedListRecursive() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    /**
+     * 
+     * @author Noah Benveniste
+     * @author Brian Wu
+     */
+    private class ListNode {
+        /** */
+        private E data;
+        /** */
+        private ListNode next;
+    }
+
+}
